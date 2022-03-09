01976fb8b04ce286fa18f36994e06132d60e9ffb
Muriel Silveira <murielkong@gmail.com>
2022-03-09 22:12:17 +0100

    feat(guessing-game): get user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-  println!("Hello, world!");
+  guessing_game();
+}
+
+fn guessing_game() {
+  println!("This is a guessing game, you need to guess a number from 1 to 10!");
+
+  println!("Input your guess:");
+
+  let mut guess = String::new();
+  io::stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read line");
+
+  println!("Your guess is: {}", guess);
 }
