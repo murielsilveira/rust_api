16ccea6ab1106638c5129bb553420e2dd66508f2
Muriel Silveira <murielkong@gmail.com>
2023-11-15 15:38:57 +0100

    chore: install rocket and create a sample endpoint

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-  println!("Hello, world!");
+use rocket::{self, get, launch, routes, Build, Rocket};
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+  rocket::build().mount("/", routes![root])
+}
+
+#[get("/")]
+fn root() -> Result<&'static str, &'static str> {
+  Ok("&'static str")
 }
